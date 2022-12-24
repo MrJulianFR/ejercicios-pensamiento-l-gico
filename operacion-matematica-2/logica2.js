301d187b7ffa39fc29530cb5b9e21734a3b51eec
Jafr200 <julianandredflorezrios@gmail.com>
2022-12-24 10:25:25 -0500

    añadimos el segundo ejercicio lógico

---
+++ operacion-matematica-2/logica2.js
@@ -0,0 +1,25 @@
+/*
+Descripción del reto
+Encuentra la lógica en las siguientes operaciones y números:
+
+3 = 27
+4 = 36
+5 = 45
+6 = 54
+7 = 63
+8 = 72
+9 = ??
+*/
+
+/* 
+El resultado del 9 es 81. La lógica detrás de este resultado, es 
+el resultado de la multiplicación del valor por 9.
+3*9 =27
+4*9 =36
+...
+*/
+
+
+function multiplicación(value){
+    return value * 9
+}
